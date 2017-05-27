e02327b997406e69376a46e98898176bc1753c9d
Ryan Winchester <code@ryanwinchester.ca>
2017-05-27 11:08:12 -0700

    Rearrange functions alphabetically

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Parallex
 
-**TODO: Add description**
+Concurrent versions of some common `Enum` functions.
+
+These might be useful if the applied function is calculation heavy.
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `parallex` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `parallex` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -13,7 +14,5 @@ def deps do
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/parallex](https://hexdocs.pm/parallex).
+Docs be found at [https://hexdocs.pm/parallex](https://hexdocs.pm/parallex).
 
--- lib/parallex.ex
+++ lib/parallex.ex
@@ -1,30 +1,15 @@
 defmodule Parallex do
   @moduledoc """
   Concurrent versions of some common `Enum` functions.
-  These might be useful if the applied function is heavy.
+  These might be useful if the applied function is calculation heavy.
   """
-
-  @doc """
-  Perform an `Enum.map/2` function using concurrent processes.
-
-  ## Examples
-
-      iex> Parallex.pmap([1, 2, 3, 4], &(&1*&1))
-      [1, 4, 9, 16]
-
-  """
-  def pmap(collection, func) do
-    collection
-    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
-    |> Enum.map(&Task.await/1)
-  end
 
   @doc """
   Perform an `Enum.filter/2` function using concurrent processes.
 
   ## Examples
 
-      iex> Parallex.pfilter([1, 2, 3, 4], &(&1 < 4))
+      iex> Parallex.pfilter([1, 2, 3, 4], fn x -> x < 4 end)
       [1, 2, 3]
 
   """
@@ -40,5 +25,20 @@ defmodule Parallex do
         end
       end)
     |> Enum.reverse
+  end
+
+  @doc """
+  Perform an `Enum.map/2` function using concurrent processes.
+
+  ## Examples
+
+      iex> Parallex.pmap([1, 2, 3, 4], fn x -> x*x end)
+      [1, 4, 9, 16]
+
+  """
+  def pmap(collection, func) do
+    collection
+    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
+    |> Enum.map(&Task.await/1)
   end
 end
